18352b09df2223287890eb5cc202a80f344d4698
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2022-05-22 03:27:29 +0200

    text escape restores content

--- frontend/src/components/details/DetailsText.tsx
+++ frontend/src/components/details/DetailsText.tsx
@@ -1,8 +1,21 @@
-import { ActionIcon, InputWrapper, Text, Textarea } from "@mantine/core"
+import {
+  ActionIcon,
+  Center,
+  Group,
+  InputWrapper,
+  Paper,
+  Popper,
+  Text,
+  Textarea,
+  Tooltip,
+  UnstyledButton,
+  useMantineTheme,
+} from "@mantine/core"
 import { useClickOutside, useClipboard } from "@mantine/hooks"
 import { showNotification } from "@mantine/notifications"
-import { FC, useEffect, useState } from "react"
-import { Copy, Edit } from "tabler-icons-react"
+import { FC, RefObject, useEffect, useRef, useState } from "react"
+import { ArrowBack, ArrowForward, Copy, Edit } from "tabler-icons-react"
+import useStateWithHistory from "../../hooks/useStateWithHistory"
 
 const alertUser = (e: BeforeUnloadEvent) => {
   e.preventDefault()
@@ -28,10 +41,14 @@ const DetailsText: FC<DetailsTextProps> = ({
   disabled,
   required,
 }) => {
-  const [val, setVal] = useState<string>(
+  const theme = useMantineTheme()
+  const [val, setVal] = useState(
     value ? value : initialValue ? initialValue : ""
   )
+  const [valHistory, setValHistory, { back, forward, history, pointer }] =
+    useStateWithHistory(val, { capacity: 2 })
   const [active, setActive] = useState<boolean>(false)
+
   const activate = () => {
     setActive(true)
     window.addEventListener("beforeunload", alertUser)
@@ -39,10 +56,12 @@ const DetailsText: FC<DetailsTextProps> = ({
 
   const deactivate = () => {
     setActive(false)
-    val !== value && onSubmit && onSubmit(val)
+    if (val !== value) {
+      onSubmit && onSubmit(val)
+      setValHistory(val)
+    }
     window.removeEventListener("beforeunload", alertUser)
   }
-  // const ref = useClickOutside(deactivate)
   const clipboard = useClipboard()
 
   useEffect(() => {
@@ -52,26 +71,48 @@ const DetailsText: FC<DetailsTextProps> = ({
   }, [])
 
   useEffect(() => {
-    value && setVal(value)
+    if (value) {
+      setVal(value)
+      setValHistory(value)
+    }
   }, [value])
 
-  const onChangeTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const onChangeTextarea = (e: React.ChangeEvent<any>) => {
     setVal(e.target.value)
     onChange && onChange(e.target.value)
   }
 
-  const onKeyDownTextarea = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+  const onKeyDownTextarea = (e: React.KeyboardEvent<any>) => {
     if (e.code == "Enter" && !e.shiftKey) {
       deactivate()
     }
+    if (e.code == "Escape") {
+      setActive(false)
+      if (val !== value) {
+        setValHistory(val)
+      }
+      window.removeEventListener("beforeunload", alertUser)
+      back()
+      setVal(valHistory)
+    }
   }
 
-  const onFocusTextarea = (
-    e: React.FocusEvent<HTMLTextAreaElement, Element>
-  ) => {
+  const onFocusTextarea = (e: React.FocusEvent<any, Element>) => {
     e.target.selectionStart = e.target.value.length
   }
 
+  const handleBlur = (e: React.FocusEvent<any, Element>) => {
+    const currentTarget = e.currentTarget
+
+    // Check the newly focused element in the next tick of the event loop
+    setTimeout(() => {
+      // Check if the new activeElement is a child of the original container
+      if (!currentTarget.contains(document.activeElement)) {
+        // You can invoke a callback or add custom logic here
+        deactivate()
+      }
+    }, 0)
+  }
   return (
     <InputWrapper
       label={
@@ -102,17 +143,113 @@ const DetailsText: FC<DetailsTextProps> = ({
       // style={{ position: "relative" }}
     >
       {active ? (
-        <Textarea
-          // ref={ref}
-          autosize
-          autoFocus
-          minRows={1}
-          value={val}
-          onChange={onChangeTextarea}
-          onKeyDown={onKeyDownTextarea}
-          onFocus={onFocusTextarea}
-          onBlur={deactivate}
-        />
+        <div
+          style={{ position: "relative" }}
+          // onBlur={handleBlur}
+        >
+          <Textarea
+            autosize
+            autoFocus
+            minRows={1}
+            value={val}
+            onChange={onChangeTextarea}
+            onKeyDown={onKeyDownTextarea}
+            onFocus={onFocusTextarea}
+            onBlur={deactivate}
+          />
+          {/* history component
+          {(pointer > 0 || pointer < history.length - 1) && (
+            <Paper
+              sx={{
+                backgroundColor:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.dark[5]
+                    : theme.colors.gray[1],
+                position: "absolute",
+                bottom: -58,
+                left: 0,
+                zIndex: 1000,
+                "&::before": {
+                  content: "''",
+                  position: "absolute",
+                  height: 8,
+                  width: 8,
+                  top: -4,
+                  left: 16,
+                  backgroundColor:
+                    theme.colorScheme === "dark"
+                      ? theme.colors.dark[5]
+                      : theme.colors.gray[1],
+                  borderRadius: 1,
+                  transform: "rotate(45deg)",
+                },
+              }}
+            >
+              {pointer > 0 && (
+                <Tooltip
+                  label="Cofnij"
+                  position="bottom"
+                  openDelay={1000}
+                  withArrow
+                >
+                  <UnstyledButton
+                    p="sm"
+                    sx={{
+                      "&:hover::before": {
+                        content: "''",
+                        position: "absolute",
+                        height: 32,
+                        width: 32,
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%,-50%)",
+                        backgroundColor: "rgba(0,0,0,0.3)",
+                        borderRadius: theme.radius.md,
+                      },
+                    }}
+                    onClick={() => {
+                      back()
+                      setVal(valHistory)
+                    }}
+                  >
+                    <ArrowBack />
+                  </UnstyledButton>
+                </Tooltip>
+              )}
+              {pointer < history.length - 1 && (
+                <Tooltip
+                  label="Przywróć"
+                  position="bottom"
+                  openDelay={1000}
+                  withArrow
+                >
+                  <UnstyledButton
+                    p="sm"
+                    sx={{
+                      "&:hover::before": {
+                        content: "''",
+                        position: "absolute",
+                        height: 32,
+                        width: 32,
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%,-50%)",
+                        backgroundColor: "rgba(0,0,0,0.3)",
+                        borderRadius: theme.radius.md,
+                      },
+                    }}
+                    onClick={() => {
+                      forward()
+                      setVal(valHistory)
+                    }}
+                  >
+                    <ArrowForward />
+                  </UnstyledButton>
+                </Tooltip>
+              )}
+            </Paper>
+          )} */}
+        </div>
       ) : (
         <div style={{ position: "relative" }}>
           <Text
