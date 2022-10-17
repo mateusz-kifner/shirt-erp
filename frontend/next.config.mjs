a91a7a163fe640318185225ef78c41f752a7104d
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2022-10-17 15:08:15 +0200

    verify env vars

---
+++ frontend/next.config.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+import { env } from "./src/env/server.mjs"
+
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config
+}
+
+export default defineNextConfig({
+  reactStrictMode: true,
+  swcMinify: true,
+  crossOrigin: "anonymous",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/erp/tasks",
+        permanent: true,
+      },
+    ]
+  },
+  // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
+  // i18n: {
+  //   locales: ["pl"],
+  //   defaultLocale: "pl",
+  // },
+})
