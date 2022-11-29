75b2e9d586658b4a3959ff08016d2e4ff213c2ee
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2022-11-29 22:43:55 +0100

    add tailwind & headless ui

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -15,6 +15,17 @@ module.exports = [
       },
     },
   },
+  // NOT working with ct8 nginx
+
+  // {
+  //   name: "strapi::cors",
+  //   config: {
+  //     origin: ["*"],
+  //     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+  //     headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+  //     keepHeaderOnError: true,
+  //   },
+  // },
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
---
+++ frontend/src/components/input/InputText.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const InputText = () => {
+  return <div>InputText</div>
+}
+
+export default InputText
