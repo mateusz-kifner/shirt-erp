61acef2418ffa8b68cc649208d202976bb855294
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2022-12-06 12:54:06 +0100

    delete certs

--- certs/create_ssl_cert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-openssl req -newkey rsa:4096 \
-            -x509 \
-            -sha256 \
-            -days 3650 \
-            -nodes \
-            -out cert.crt \
-            -keyout cert.key \
-            -subj "/C=PL/ST=Pomorskie/L=Gdynia/O=ShirtDip/OU=Printing Software Manufacturer/CN=shirtdip.pl"
