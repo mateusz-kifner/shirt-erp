d31b16f032a672510bb6902ba601cf3fa10df8a5
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2023-07-03 04:31:16 +0200

    move frontend to ~

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: path.join(__dirname, "tsconfig.json"),
+      },
+      rules: {
+        "@typescript-eslint/no-unsafe-member-access": "warn",
+        "@typescript-eslint/no-unsafe-call": "warn",
+        "@typescript-eslint/no-unsafe-assignment": "warn",
+        "@typescript-eslint/restrict-plus-operands": "warn",
+        "@typescript-eslint/restrict-template-expressions": "off",
+        "@typescript-eslint/no-unsafe-argument": "warn",
+        "@next/next/no-img-element": "off",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: path.join(__dirname, "tsconfig.json"),
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/ban-ts-comment": "warn",
+  },
+};
+
+module.exports = config;
---
+++ src/pages/erp/logs/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react"
+
+import template from "../../../models/log.model.json"
+import { makeDefaultListItem } from "../../../components/DefaultListItem"
+import DefaultPage from "../../../components/DefaultPage"
+
+const LoggerPage: FC = () => {
+  return (
+    <DefaultPage
+      template={template}
+      entryName="logs"
+      ListElement={makeDefaultListItem("message", "type")}
+    />
+  )
+}
+
+export default LoggerPage
