debf8835414e9ef9961f8610823c1f23a9d273df
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2023-07-03 04:48:02 +0200

    initial setup for shadcn-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,12 @@
-import { type Config } from "tailwindcss";
-import { type PluginAPI } from "tailwindcss/types/config";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
     container: {
       center: true,
@@ -55,53 +58,19 @@ export default {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: "0" },
+          from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-        pop: {
-          "0%": {
-            transform: "scale( 0.95)",
-          },
-          "40%": {
-            transform: "scale(1.02)",
-          },
-          "100%": {
-            transform: "scale(1)",
-          },
-        },
-        show: {
-          from: { opacity: "0" },
-          to: { opacity: "1" },
-        },
-        contentShow: {
-          from: {
-            opacity: "0",
-            transform: "translate(-50%, -48%) scale(0.96)",
-          },
-          to: { opacity: "1", transform: "translate(-50%, -50%) scale(1)" },
+          to: { height: 0 },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        pop: "pop  0.25s cubic-bezier(0.16, 1, 0.3, 1)",
-        show: "show 150ms cubic-bezier(0.16, 1, 0.3, 1)",
-        contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
-      },
-      opacity: {
-        15: ".15",
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    function ({ addVariant }: PluginAPI) {
-      addVariant("child", "& > *");
-      addVariant("child-hover", "& > *:hover");
-    },
-  ],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate")],
+}
