6cfa9c30e50cd64a5127ef2c4d7bcc18658af8be
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2023-08-26 22:54:43 +0200

    remove old schemas

--- src/schema/designSchema.tsx
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const designSchema = z.object({
-  id: z.number(),
-  name: z.string().max(1024).nullable().optional(),
-  data: z.record(z.string(), z.any()).array().array(),
-});
-
-export type DesignType = z.infer<typeof designSchema>;
