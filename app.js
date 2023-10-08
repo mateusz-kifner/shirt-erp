87c429a837d132a10431928f16b19c0df81f3101
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2023-10-08 21:29:02 +0200

    Upgrade to nextjs 13

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 // start app via Phusion Passenger
-const cli = require("next/dist/cli/next-start");
-cli.nextStart([]);
+const path = require("path");
+
+const nextPath = path.join(__dirname, "node_modules", ".bin", "next");
+
+process.argv.length = 1;
+process.argv.push(nextPath, "start");
+
+require(nextPath);
