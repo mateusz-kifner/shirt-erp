71dfef9884af3ae5cfc0e495a477abedaaf0012c
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2023-10-09 00:20:12 +0200

    fix bug decimal(2,10) - > decimal(10,2)

--- src/db/schema/expenses.ts
+++ src/db/schema/expenses.ts
@@ -4,7 +4,7 @@ import { metadata } from "./_metadata";
 export const expenses = pgTable("expenses", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 255 }),
-  cost: decimal("cost", { precision: 2, scale: 10 }),
+  cost: decimal("cost", { precision: 10, scale: 2 }),
   expenseData: json("expense_data")
     .$type<{ name?: string; cost?: number }[]>()
     .default([]),
