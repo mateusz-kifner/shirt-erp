99cc8dac6b5f78ecf2fe966cb86e1ce624ccccd4
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2024-04-09 23:52:34 +0200

    Add warehouse page and assets

---
+++ src/pages/erp/warehouse/index.tsx
@@ -0,0 +1,15 @@
+interface WarehousePageProps {}
+
+function WarehousePage(props: WarehousePageProps) {
+  const {} = props;
+  return (
+    <div>
+      <img
+        src="/assets/cardboard-box-with-lid-svgrepo-com.svg"
+        className="h-64 w-64"
+      />
+    </div>
+  );
+}
+
+export default WarehousePage;
